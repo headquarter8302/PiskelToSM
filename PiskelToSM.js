8582c1a4f49c95860b5bb97b34dc6c307b8cb15d
Markus <65797058+headquarter8302@users.noreply.github.com>
2022-04-19 21:02:29 +0800

    Update PiskelToSM.js

--- PiskelToSM.js
+++ PiskelToSM.js
@@ -1,13 +1,24 @@
-const obj = JSON.parse('{"pixels":[{"col":0,"row":0,"color":4278223359},{"col":1,"row":0,"color":4278223359}],"time":1650340642455}');
+const obj = {
+  pixels: [
+    { col: 0, row: 0, color: 4278223359 },
+    { col: 1, row: 0, color: 4278223359 },
+  ],
+  time: 1650340642455,
+};
+
 var myJSON = JSON.stringify(obj.pixels);
 
-function androidToHex(color){
-  const [, alpha, ...colorArray] = ('00000000' + color.toString(16)).slice(-8).match(/([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i)
-  return `#${colorArray.join('')}${alpha}`
+function androidToHex(color) {
+  const [, , ...colorArray] = ("00000000" + color.toString(16))
+    .slice(-8)
+    .match(/([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i);
+  return `#${colorArray.join("")}`;
 }
-  function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
+function componentToHex(color) {
+  var hex = color.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
 
-  console.log(androidToHex(obj.pixels[0].color))
+console.log(androidToHex(obj.pixels[0].color));
+// Can't seem to get working
+console.log(componentToHex(obj.pixels[0].color));
